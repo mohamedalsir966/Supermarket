9d9664c10a7a9d49c882ac2e91b627ffc1ad3264
unknown <mohamedalsir966@gmail.com>
2020-05-02 06:16:42 -0800

    first commit

---
+++ Domain/Models/Product.cs
@@ -0,0 +1,18 @@
+
+using System.Text;
+using System;
+using static System.Math;
+
+namespace Supermarket.API.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public short QuantityInPackage { get; set; }
+        public  EUnitOfMeasurement UnitOfMeasurement { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Domain/Models/category.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace Supermarket.API.Domain.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Product> Products { get; set; } = new List<Product>();
+    }
+}
